3d587fa033a1bae16a0e9f110867b391c1fb0f8f
Piyush Raj <piyushraj3315@gmail.com>
2025-10-09 14:35:00 +0000

    Console Error
    ReactDOM.preload(): Expected two arguments, a non-empty

--- src/components/create-event-dialog.tsx
+++ src/components/create-event-dialog.tsx
@@ -202,7 +202,7 @@ export function CreateEventDialog({ children, eventToEdit }: { children: React.R
                 </FormItem>
               )}
             />
-            {bannerUrl && z.string().url().safeParse(bannerUrl).success && (
+            {z.string().url().safeParse(bannerUrl).success && (
               <div className="relative w-full h-48 rounded-md overflow-hidden">
                 <Image src={bannerUrl} alt="Banner preview" fill style={{objectFit: 'cover'}} />
               </div>
@@ -475,3 +475,5 @@ export function CreateEventDialog({ children, eventToEdit }: { children: React.R
     </Dialog>
   );
 }
+
+    
